197ae82cf8cbec08b3bc502f245b1255f435b2c7
Marcin Proniakin <marcinpro1@gmail.com>
2015-10-07 17:49:22 +0200

    Dialog style changes - added buttons (not finished)

--- UrbanGame/app/src/main/java/com/example/nataliajastrzebska/urbangame/ChooseCreatingModeDialog.java
+++ UrbanGame/app/src/main/java/com/example/nataliajastrzebska/urbangame/ChooseCreatingModeDialog.java
@@ -7,6 +7,7 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 
 /**
  * Created by nataliajastrzebska on 06/10/15.
@@ -17,8 +18,9 @@ public class ChooseCreatingModeDialog extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View v = inflater.inflate(R.layout.dialog_choose_creating_mode, container, false);
-        dialogStyling(getDialog());
+        //dialogStyling(getDialog());
         setStyle(DialogFragment.STYLE_NORMAL, R.style.DialogTheme);
+        getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         return v;
     }
 
