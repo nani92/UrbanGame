5be6bdd4ad38d47550d24c5be0ebef1baeb3ed88
Marcin Proniakin <marcinpro1@gmail.com>
2015-10-07 21:20:04 +0200

    Removing useless code, adjusting button size and positioning in dialog

--- UrbanGame/app/src/main/java/com/example/nataliajastrzebska/urbangame/ChooseCreatingModeDialog.java
+++ UrbanGame/app/src/main/java/com/example/nataliajastrzebska/urbangame/ChooseCreatingModeDialog.java
@@ -18,14 +18,9 @@ public class ChooseCreatingModeDialog extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View v = inflater.inflate(R.layout.dialog_choose_creating_mode, container, false);
-        //dialogStyling(getDialog());
         setStyle(DialogFragment.STYLE_NORMAL, R.style.DialogTheme);
         getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         return v;
-    }
-
-    void dialogStyling(Dialog dialog){
-        dialog.setTitle(R.string.dialog_chooseCreatingMode_title);
     }
 
 }
