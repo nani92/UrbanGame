d85755586e2f0bfc9fb58db560e27482b5cfed62
Natalia Jastrzębska <nataliak@10g.pl>
2015-10-07 11:06:24 +0200

    Created Dialog

---
+++ UrbanGame/app/src/main/java/com/example/nataliajastrzebska/urbangame/ChooseCreatingModeDialog.java
@@ -0,0 +1,29 @@
+package com.example.nataliajastrzebska.urbangame;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by nataliajastrzebska on 06/10/15.
+ */
+public class ChooseCreatingModeDialog extends DialogFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View v = inflater.inflate(R.layout.dialog_choose_creating_mode, container, false);
+        dialogStyling(getDialog());
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.DialogTheme);
+        return v;
+    }
+
+    void dialogStyling(Dialog dialog){
+        dialog.setTitle(R.string.dialog_chooseCreatingMode_title);
+    }
+
+}
--- UrbanGame/app/src/main/java/com/example/nataliajastrzebska/urbangame/MainActivity.java
+++ UrbanGame/app/src/main/java/com/example/nataliajastrzebska/urbangame/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.nataliajastrzebska.urbangame;
 
+import android.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void createGameClicked(View v){
+        showChooseCreatingModeDialog();
+    }
+
+    void showChooseCreatingModeDialog() {
+        FragmentManager fm = getSupportFragmentManager();
+        ChooseCreatingModeDialog chooseCreatingModeDialog = new ChooseCreatingModeDialog();
+        chooseCreatingModeDialog.show(fm, "dialog_choose_creating_mode");
+    }
+
 }
