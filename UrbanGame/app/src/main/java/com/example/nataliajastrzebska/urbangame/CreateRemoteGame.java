a3e8b4704254559271ffb069b362bcdb9f6fe6f5
Natalia Jastrzębska <nataliak@10g.pl>
2015-10-14 14:19:49 +0200

    Autocompleting location

--- UrbanGame/app/src/main/java/com/example/nataliajastrzebska/urbangame/CreateRemoteGame.java
+++ UrbanGame/app/src/main/java/com/example/nataliajastrzebska/urbangame/CreateRemoteGame.java
@@ -1,15 +1,103 @@
 package com.example.nataliajastrzebska.urbangame;
 
+import android.content.res.Resources;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
-public class CreateRemoteGame extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.AutocompletePrediction;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+
+public class CreateRemoteGame extends AppCompatActivity  implements GoogleApiClient.OnConnectionFailedListener{
+
+
+    protected GoogleApiClient mGoogleApiClient;
+
+    private PlaceAutocompleteAdapter mAdapter;
+
+    private AutoCompleteTextView mAutocompleteView;
+
+    private static final LatLngBounds BOUNDS = new LatLngBounds(
+            new LatLng(-34.041458, 150.790100), new LatLng(-33.682247, 151.383362));
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_remote_game);
+
+        setmGoogleApiClient();
+        setmAdapter();
+        setmAutocompleteView();
+
+        mAutocompleteView.setOnItemClickListener(mAutocompleteClickListener);
+        mAutocompleteView.setAdapter(mAdapter);
     }
 
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    private AdapterView.OnItemClickListener mAutocompleteClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            final AutocompletePrediction item = mAdapter.getItem(position);
+            final String placeId = item.getPlaceId();
+            final CharSequence primaryText = item.getPrimaryText(null);
+
+            PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi
+                    .getPlaceById(mGoogleApiClient, placeId);
+            placeResult.setResultCallback(mUpdatePlaceDetailsCallback);
+
+            Toast.makeText(getApplicationContext(), "Clicked: " + primaryText,
+                    Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    void setmGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, R.string.google_maps_key, this)
+                .addApi(Places.GEO_DATA_API)
+                .build();
+    }
+
+    void setmAdapter() {
+        mAdapter = new PlaceAutocompleteAdapter(this, mGoogleApiClient, BOUNDS,
+                null);
+    }
+
+    void setmAutocompleteView() {
+        mAutocompleteView = (AutoCompleteTextView)
+                findViewById(R.id.tv_createRemoteGameActivity_searchLocation);
+    }
+
+    private ResultCallback<PlaceBuffer> mUpdatePlaceDetailsCallback
+            = new ResultCallback<PlaceBuffer>() {
+        @Override
+        public void onResult(PlaceBuffer places) {
+            if (!places.getStatus().isSuccess()) {
+                places.release();
+                return;
+            }
+            final Place place = places.get(0);
+            places.release();
+        }
+    };
 
 }
