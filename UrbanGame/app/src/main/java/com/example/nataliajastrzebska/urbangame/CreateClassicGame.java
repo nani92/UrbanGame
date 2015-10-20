b9b40900c5ee51bfd4716c5e95bc0c2be529a932
Marcin Proniakin <marcinpro1@gmail.com>
2015-10-20 18:01:46 +0200

    Created CreateClassicGame Activity

---
+++ UrbanGame/app/src/main/java/com/example/nataliajastrzebska/urbangame/CreateClassicGame.java
@@ -0,0 +1,13 @@
+package com.example.nataliajastrzebska.urbangame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateClassicGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_classic_game);
+    }
+}
