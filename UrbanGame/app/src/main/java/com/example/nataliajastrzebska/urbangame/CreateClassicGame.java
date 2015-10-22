005b2499f26440b6a3db8231a687c63267b3c71f
Marcin Proniakin <marcinpro1@gmail.com>
2015-10-23 00:42:39 +0200

    added gps check and dialog, added marker on location button, added marker onclick(touch) removal (tmp)

--- UrbanGame/app/src/main/java/com/example/nataliajastrzebska/urbangame/CreateClassicGame.java
+++ UrbanGame/app/src/main/java/com/example/nataliajastrzebska/urbangame/CreateClassicGame.java
@@ -10,9 +10,8 @@ import android.os.Build;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
 
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,23 +19,27 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class CreateClassicGame extends AppCompatActivity
-        implements GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, LocationListener {
+//TO FIX - network and gps dialogs
+
+public class CreateClassicGame extends AppCompatActivity implements  OnMapReadyCallback,  LocationListener, GoogleMap.OnMarkerClickListener {
 
     protected GoogleApiClient mGoogleApiClient;
     private GoogleMap mMap;
     private Location mPositionNetwork;
     private Location mPositionGPS;
+    private LatLng myPosition;
     private LocationManager mLocationManager;
-
+    View view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_classic_game);
 
-        checkInternetConnection();
+        mLocationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+        checkInternetAndGpsConnection();
 
         setMap();
 
@@ -44,15 +47,11 @@ public class CreateClassicGame extends AppCompatActivity
 
     }
 
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-    }
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {}
-    @Override
-    public void onConnected(Bundle bundle) {}
-    @Override
-    public void onConnectionSuspended(int i) {}
+
     @Override
     public void onLocationChanged(Location location) {
         getLocation();
@@ -62,14 +61,21 @@ public class CreateClassicGame extends AppCompatActivity
     public void onStatusChanged(String provider, int status, Bundle extras) {}
 
     @Override
-    public void onProviderEnabled(String provider) {}
+    public void onProviderEnabled(String provider) {
+        getLocation();
+        showMyPosition();}
 
     @Override
-    public void onProviderDisabled(String provider) {}
+    public void onProviderDisabled(String provider) {checkInternetAndGpsConnection();}
 
-    void checkInternetConnection() {
-        if (!Services.getInstance().isNetworkAvailable(this)) {//is this necessary????
+
+
+    void checkInternetAndGpsConnection() {
+        if (Services.getInstance().isNetworkAvailable(this)) {
             showEnableInternetModeDialog();
+        }
+        if(!mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            showEnableGPSModeDialog();
         }
     }
 
@@ -79,7 +85,10 @@ public class CreateClassicGame extends AppCompatActivity
         enableInternetDialog.show(fm, "dialog_enable_internet");
     }
 
-    void showEnableGPSModeDialog() {//todo
+    void showEnableGPSModeDialog() {
+        FragmentManager fm = getSupportFragmentManager();
+        EnableGpsDialog enableGpsDialog = new EnableGpsDialog();
+        enableGpsDialog.show(fm, "dialog_enable_gps");
     }
 
 
@@ -87,10 +96,11 @@ public class CreateClassicGame extends AppCompatActivity
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map_creatingClassicGame);
         mMap = mapFragment.getMap();
         mapFragment.getMapAsync(this);
+        mMap.setOnMarkerClickListener(this);
     }
 
     void setLocationUpdates() {
-        mLocationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 // TODO: Consider calling
@@ -103,8 +113,8 @@ public class CreateClassicGame extends AppCompatActivity
                 return;
             }
         }
-        mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 60000, 15, this);
-        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,60000, 15, this);
+        mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 60000, 10, this);
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 60000, 10, this);
 
     }
 
@@ -126,24 +136,29 @@ public class CreateClassicGame extends AppCompatActivity
         if(mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
             mPositionGPS = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-    }
-    void showMyPosition() {
-        LatLng myPosition = null;
         if(mPositionNetwork != null) {
             myPosition = new LatLng(mPositionNetwork.getLatitude(), mPositionNetwork.getLongitude());
         }
         if(mPositionGPS != null) {
             myPosition = new LatLng(mPositionGPS.getLatitude(), mPositionGPS.getLongitude());
         }
-        if (myPosition != null) {
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(myPosition));
-        mMap.moveCamera(CameraUpdateFactory.zoomBy(15));
-        mMap.addMarker(new MarkerOptions().position(myPosition));
-        }
-
-        //Toast toast = Toast.makeText(getApplicationContext(),"Unable to receive position",Toast.LENGTH_SHORT);
-        //toast.show();
 
     }
+    void showMyPosition() {
+        if (myPosition != null) {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myPosition, 15));
+            mMap.addMarker(new MarkerOptions().position(myPosition));
+        }
+    }
 
+    public void putMarkerOnMyPosition(View view){
+        getLocation();
+        mMap.addMarker(new MarkerOptions().position(myPosition));
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        marker.remove();
+        return true;
+    }
 }
--- UrbanGame/app/src/main/java/com/example/nataliajastrzebska/urbangame/EnableGpsDialog.java
+++ UrbanGame/app/src/main/java/com/example/nataliajastrzebska/urbangame/EnableGpsDialog.java
@@ -2,7 +2,6 @@ package com.example.nataliajastrzebska.urbangame;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import android.provider.Settings;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
@@ -14,20 +13,20 @@ import android.widget.Button;
 /**
  * Created by nataliajastrzebska on 19/10/15.
  */
-public class EnableInternetDialog extends DialogFragment implements View.OnClickListener {
+public class EnableGpsDialog extends DialogFragment implements View.OnClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        View v = inflater.inflate(R.layout.dialog_enable_internet, container, false);
-        setStyle(android.support.v4.app.DialogFragment.STYLE_NORMAL, R.style.DialogTheme);
+        View v = inflater.inflate(R.layout.dialog_enable_gps, container, false);
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.DialogTheme);
         getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         getDialog().getWindow().setBackgroundDrawableResource(R.drawable.dialog_background);
 
 
-        Button btnOk=(Button)v.findViewById(R.id.btn_enableInternetDialog_OK);
+        Button btnOk=(Button)v.findViewById(R.id.btn_enableGPSDialog_OK);
         btnOk.setOnClickListener(this);
-        Button btnCancel=(Button)v.findViewById(R.id.btn_chooseEnableInternetDialog_CANCEL);
+        Button btnCancel=(Button)v.findViewById(R.id.btn_chooseEnableGPSDialog_CANCEL);
         btnCancel.setOnClickListener(this);
 
         return v;
@@ -35,8 +34,8 @@ public class EnableInternetDialog extends DialogFragment implements View.OnClick
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.btn_enableInternetDialog_OK){
-            startActivityForResult(new Intent(Settings.ACTION_WIFI_SETTINGS), 0);
+        if (v.getId() == R.id.btn_enableGPSDialog_OK){
+            startActivityForResult(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS), 0);
         }
         dismiss();
     }
