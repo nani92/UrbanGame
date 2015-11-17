26005aa5f148c54bab500da248b06c10baf56a36
Natalia Jastrzębska <nataliak@10g.pl>
2015-11-17 12:11:08 +0100

    Added camera service to CardboardActivity

--- UrbanGame/app/src/main/java/com/example/nataliajastrzebska/urbangame/createTaskActivites/TaskFindAndAnswer.java
+++ UrbanGame/app/src/main/java/com/example/nataliajastrzebska/urbangame/createTaskActivites/TaskFindAndAnswer.java
@@ -2,6 +2,7 @@ package com.example.nataliajastrzebska.urbangame.createTaskActivites;
 
 import android.app.Activity;
 import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
 import android.opengl.GLES20;
 import android.opengl.Matrix;
 import android.os.Bundle;
@@ -12,12 +13,21 @@ import com.google.vrtoolkit.cardboard.*;
 
 import com.example.nataliajastrzebska.urbangame.R;
 
+import java.io.IOException;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 public class TaskFindAndAnswer extends CardboardActivity implements CardboardView.StereoRenderer, SurfaceTexture.OnFrameAvailableListener {
 
     CardboardView cardboardView;
+    private SurfaceTexture surface;
+    private Camera camera;
+    private int texture;
+    private static final int GL_TEXTURE_EXTERNAL_OES = 0x8D65;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,15 +63,50 @@ public class TaskFindAndAnswer extends CardboardActivity implements CardboardVie
 
     @Override
     public void onSurfaceCreated(EGLConfig eglConfig) {
+        texture = createTexture();
+        startCamera(texture);
     }
 
-    private int loadGLShader(int type, String code) {
-    }
 
     @Override
     public void onRendererShutdown() {
 
     }
-[]
+
+    public void startCamera(int texture)
+    {
+        surface = new SurfaceTexture(texture);
+        surface.setOnFrameAvailableListener(this);
+
+        camera = Camera.open();
+
+        try
+        {
+            camera.setPreviewTexture(surface);
+            camera.startPreview();
+        }
+        catch (IOException ioe)
+        {
+            Log.w("MainActivity","CAM LAUNCH FAILED");
+        }
+    }
+
+    static private int createTexture()
+    {
+        int[] texture = new int[1];
+
+        GLES20.glGenTextures(1,texture, 0);
+        GLES20.glBindTexture(GL_TEXTURE_EXTERNAL_OES, texture[0]);
+        GLES20.glTexParameterf(GL_TEXTURE_EXTERNAL_OES,
+                GL10.GL_TEXTURE_MIN_FILTER,GL10.GL_LINEAR);
+        GLES20.glTexParameterf(GL_TEXTURE_EXTERNAL_OES,
+                GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
+        GLES20.glTexParameteri(GL_TEXTURE_EXTERNAL_OES,
+                GL10.GL_TEXTURE_WRAP_S, GL10.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GL_TEXTURE_EXTERNAL_OES,
+                GL10.GL_TEXTURE_WRAP_T, GL10.GL_CLAMP_TO_EDGE);
+
+        return texture[0];
+    }
 
 }
