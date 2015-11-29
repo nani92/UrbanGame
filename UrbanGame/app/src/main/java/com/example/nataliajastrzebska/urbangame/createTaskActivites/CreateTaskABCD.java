b95eac278bc9a4e310354f8461feb66dd0c1ab97
Natalia Jastrzębska <nataliak@10g.pl>
2015-11-29 17:50:00 +0100

    Added activ for abcd

---
+++ UrbanGame/app/src/main/java/com/example/nataliajastrzebska/urbangame/createTaskActivites/CreateTaskABCD.java
@@ -0,0 +1,15 @@
+package com.example.nataliajastrzebska.urbangame.createTaskActivites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.nataliajastrzebska.urbangame.R;
+
+public class CreateTaskABCD extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_task_abcd);
+    }
+}
